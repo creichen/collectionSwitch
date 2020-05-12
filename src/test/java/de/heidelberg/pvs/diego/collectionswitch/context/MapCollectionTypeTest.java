8cfa903813f85d2c0443293b45126fc7d037376d
Noric Couderc <noric.couderc@cs.lth.se>
2020-05-12 18:40:31 +0200

    Changed expected class
    from Google's ArrayMap to ours.

--- src/test/java/de/heidelberg/pvs/diego/collectionswitch/context/MapCollectionTypeTest.java
+++ src/test/java/de/heidelberg/pvs/diego/collectionswitch/context/MapCollectionTypeTest.java
@@ -19,47 +19,50 @@ import net.openhft.koloboke.collect.map.hash.HashObjObjMap;
 import vlsi.utils.CompactHashMap;
 
 public class MapCollectionTypeTest {
-	
-	@Test
-	public void testSimpleCreation() throws Exception {
-		
-		
-		MapCollectionType type = MapCollectionType.FASTUTILS_HASHMAP;
-		Map<?,?> map = type.createMap();
-		Assert.assertTrue(map instanceof THashMap);
-		
-		type = MapCollectionType.GOOGLE_ARRAYMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof ArrayMap);
-		
-		type = MapCollectionType.GSCOLLECTIONS_UNIFIEDMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof UnifiedMap);
-		
-		type = MapCollectionType.JDK_HASHMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof HashMap);
-		
-		type = MapCollectionType.JDK_LINKEDHASHMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof LinkedHashMap);
-		
-		type = MapCollectionType.KOLOBOKE_HASHMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof HashObjObjMap);
-		
-		type = MapCollectionType.NAYUKI_COMPACTHASHMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof CompactHashMap);
-		
-		type = MapCollectionType.NLP_ARRAYMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof edu.stanford.nlp.util.ArrayMap);
-		
-		type = MapCollectionType.ONLINEADAPTER_ADAPTIVEMAP;
-		map = type.createMap();
-		Assert.assertTrue(map instanceof AdaptiveMap);
-		
-	}
+
+        @Test
+        public void testSimpleCreation() throws Exception {
+
+
+                MapCollectionType type = MapCollectionType.FASTUTILS_HASHMAP;
+                Map<?,?> map = type.createMap();
+                Assert.assertTrue(map instanceof THashMap);
+
+                type = MapCollectionType.GOOGLE_ARRAYMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof ArrayMap);
+
+                type = MapCollectionType.GSCOLLECTIONS_UNIFIEDMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof UnifiedMap);
+
+                type = MapCollectionType.JDK_HASHMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof HashMap);
+
+                type = MapCollectionType.JDK_LINKEDHASHMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof LinkedHashMap);
+
+                type = MapCollectionType.KOLOBOKE_HASHMAP;
+                map = type.createMap();
+
+                System.out.println("LOOK AT ME");
+                System.out.println(map.getClass());
+                Assert.assertTrue(map instanceof HashObjObjMap);
+
+                type = MapCollectionType.NAYUKI_COMPACTHASHMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof CompactHashMap);
+
+                type = MapCollectionType.NLP_ARRAYMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof de.heidelberg.pvs.diego.collectionswitch.nlp.ArrayMap);
+
+                type = MapCollectionType.ONLINEADAPTER_ADAPTIVEMAP;
+                map = type.createMap();
+                Assert.assertTrue(map instanceof AdaptiveMap);
+
+        }
 
 }
