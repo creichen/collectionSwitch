d0ed3a8f7ce4b1ea3c121a21c513b2a2f2c4a871
Noric Couderc <noric.couderc@cs.lth.se>
2020-05-14 13:17:10 +0200

    Added methods to MapAllocationContext
    These return the expected interface

--- src/main/java/de/heidelberg/pvs/diego/collectionswitch/context/MapAllocationContext.java
+++ src/main/java/de/heidelberg/pvs/diego/collectionswitch/context/MapAllocationContext.java
@@ -2,15 +2,41 @@ package de.heidelberg.pvs.diego.collectionswitch.context;
 
 import java.util.Map;
 
+import se.lth.util.*;
+import se.lth.util.concurrent.*;
+
 public interface MapAllocationContext  extends AllocationContextUpdatable<MapCollectionType> {
-	
-	public <K, V> Map<K, V> createMap();
 
-	public <K, V> Map<K, V> createMap(int initialCapacity);
-	
-	public <K, V> Map<K, V> createMap(int initialCapacity, float loadFactor);
+        public <K, V> Map<K, V> createMap();
 
-	public <K, V> Map<K, V> createMap(Map<K, V> map);
-	
+        public <K, V> Map<K, V> createMap(int initialCapacity);
+
+        public <K, V> Map<K, V> createMap(int initialCapacity, float loadFactor);
+
+        public <K, V> Map<K, V> createMap(Map<K, V> map);
+
+        public <K, V> HashMapInterface createHashMapInterface( );
+
+        public <K, V> HashMapInterface createHashMapInterface(int initialCapacity);
+
+        public <K, V> HashMapInterface createHashMapInterface(Map<K, V> mapToCopy);
+
+        public <K, V> TreeMapInterface createTreeMapInterface( );
+
+        public <K, V> TreeMapInterface createTreeMapInterface(int initialCapacity);
+
+        public <K, V> TreeMapInterface createTreeMapInterface(Map<K, V> mapToCopy);
+
+        public <K, V> LinkedHashMapInterface createLinkedHashMapInterface( );
+
+        public <K, V> LinkedHashMapInterface createLinkedHashMapInterface(int initialCapacity);
+
+        public <K, V> LinkedHashMapInterface createLinkedHashMapInterface(Map<K, V> mapToCopy);
+
+        public <K, V> ConcurrentSkipListMapInterface createConcurrentSkipListMapInterface( );
+
+        public <K, V> ConcurrentSkipListMapInterface createConcurrentSkipListMapInterface(int initialCapacity);
+
+        public <K, V> ConcurrentSkipListMapInterface createConcurrentSkipListMapInterface(Map<K, V> mapToCopy);
 
 }
